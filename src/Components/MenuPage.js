78de782d40dfc75ed90f6f5fc8ab5f19d72435f5
KIMBERLY_TAPFUMANEYI_KIMBERLY <kimberlytapfumanyi16@gmail.com>
2024-06-17 22:39:25 +0200

    MENU

--- src/Components/MenuPage.js
+++ src/Components/MenuPage.js
@@ -1,43 +1,57 @@
 import React from 'react';
-import  from '.strawberrycake-3411553_1280.jpg'
-
+import FLACKYPASTRY from '../images/strawberrycake-3411553_1280.jpg';
+import SWISSROLL from '../images/dessert-678071_1280.jpg;'
+import CREAMPUFF from '../images/cream-puff-1694333_1280.jpg';
+import CAURE from '../images/cakes-3032323_1280.jpg'
+import BREAD from '../images/bread-8814788_1280.jpg'
+import CROISSANT from '../images/bread-1284438_1920.jpg'
+import CAKE from '../images/dessert-6898136_1280.jpg'
+import DANISHPASRY from '../images/danish-pastry-892909_1280.jpg'
+import KIMO from '../images/bread-8814788_1280.jpg'
+import CHOUX from '../images/cake-3744058_1280.jpg'
 const MenuPage = () => {
   return (
     <div>
       <h1>Our Menu</h1>
       <ul>
         <li>
-          <img src="src/images/strawberrycake-3411553_1280.jpg" alt="Pastry 1" />
+          <img src={FLACKYPASTRY} alt="Flaky Pastry" />
           <p>FLACKY PASTRY</p>
         </li>
         <li>
-          <img src="src/images/dessert-678071_1280.jpg" alt="Pastry 2" />
+          <img src={SWISSROLL} alt="SWISSROLL" />
           <p>SWISSROLL</p>
         </li>
         <li>
-          <img src="src/images/cream-puff-1694333_1280.jpg" alt="Pastry 3" />
+          <img src={CREAMPUFF} alt="CREAMPUFF" />
           <p>CREAM PUFF</p>
         </li>
         <li>
-          <img src="src/images/cakes-3032323_1280.jpg" alt="Pastry 4" />
+          <img src={CAURE} alt="CAU`RE" />
           <p>CAU`RE</p>
         </li>
         <li>
-          <img src="src/images/bread-8814788_1280.jpg" alt="Pastry 5" />
+          <img src={BREAD} alt="BREAD" />
           <p>BREAD</p>
         </li>
         <li>
-          <img src="src/images/bread-1284438_1920.jpg" alt="Pastry 5" />
+          <img src={CROISSANT} alt="CROISSANT" />
           <p>CROISSANT</p>
         </li><li>
-          <img src="src/images/baked-goods-1846460_1920.jpg" alt="Pastry 5" />
+          <img src={CAKE} alt="CAKE" />
           <p>CAKE</p>
-        </li><li>
-          <img src="src/images/dessert-6898136_1280.jpg" alt="Pastry 5" />
-          <p>DOU`FU</p>
-        </li><li>
-          <img src="src/images/bread-8814788_1280.jpg" alt="Pastry 5" />
-          <p>BREAD</p>
+        </li>
+        <li>
+          <img src={DANISHPASRY} alt="DANISH PASTRY" />
+          <p>DANISHPASRY</p>
+        </li>
+        <li>
+          <img src={KIMO} alt="KIMO" />
+          <p>KIMO</p>
+        </li>
+        <li>
+          <img src={CHOUX} alt="CHOUX" />
+          <p>CHOUX</p>
         </li>
       </ul>
     </div>
